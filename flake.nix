18e0020830251e7686cbe7d9a8288d82d9cccd23
Nicholas <3789764+skykanin@users.noreply.github.com>
2024-11-20 14:44:21 +0100

    Make services use new dapla schema (#176)
    * vscode-python: use new dapla schema
    
    * jupyter: use new dapla schema
    
    * add dev env using nix flake
    
    * rstudio: use new dapla schema
    
    * jupyter-playground: use new dapla schema
    
    * jupyter-pyspark: use new dapla schema

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Dev environment for Dapla Lab helm-charts";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      perSystem = {
+        pkgs,
+        ...
+      }: {
+        devShells.default = pkgs.mkShell {
+          name = "Dapla Lab helm charts";
+          packages = with pkgs; [
+            nixd
+            vscode-langservers-extracted
+            yaml-language-server
+          ];
+        };
+
+        formatter = pkgs.alejandra;
+      };
+    };
+}
